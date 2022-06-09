d79c15ee5f17fed949e41e25a05f16993ff8cc48
amenu4aie2 <amenu4aie21177@am.students.amrita.edu>
2022-06-09 06:32:56 +0530

    9th

--- My project (1)/Assets/scripts/deletion insertion movement.cs
+++ My project (1)/Assets/scripts/deletion insertion movement.cs
@@ -7,21 +7,46 @@ public class deletioninsertionmovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+  void OnMouseDown() {
+            destroy(gameobject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //destroy object on keydown
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
-            GameObject[] gos;
-            gos = GameObject.FindGameObjectsWithTag("cube");
-            foreach (GameObject go in gos)
+            Destroy(gameObject);
+        }
+        //detecting collisionEnter
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
             {
-                Destroy(go);
+                Destroy(gameObject);
             }
+        }
+        //moving gameobject using if condition and velocity
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 10);
+        } 
+        
+
+        //if key pressed Adding force to the object
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*500);
+        }
+        if (Input.GetKeyDown(KeyCode.w))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward*500);
+        }
+        if (Input.GetKeyDown(KeyCode.s))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back*500);
         }
 
     }
